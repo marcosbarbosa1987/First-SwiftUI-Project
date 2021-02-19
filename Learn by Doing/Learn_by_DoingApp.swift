1743f6a37502c4e7f4f73e7738caca4ab1d60525
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-02-19 11:06:43 -0300

    First commit

---
+++ Learn by Doing/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Learn by Doing
+//
+//  Created by Marcos Barbosa on 19/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
