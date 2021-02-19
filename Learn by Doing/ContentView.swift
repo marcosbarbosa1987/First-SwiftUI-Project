147a0b4c140f164a7b3bbf286d0f539738bb6649
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-02-19 14:56:05 -0300

    Create launcherScreen;

--- Learn by Doing/ContentView.swift
+++ Learn by Doing/ContentView.swift
@@ -8,11 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - Properties
+    
+    // MARK: - Body
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
+
+// MARK: - Preview
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
